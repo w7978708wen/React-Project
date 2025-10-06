8e4e2735385704a847919b79e1a1e2105204f6c2
w7595wen <wingchee8250@gmail.com>
2025-10-06 20:55:58 +1100

    got rid of the right pics from the right component

--- vite-project/src/Component3.jsx
+++ vite-project/src/Component3.jsx
@@ -4,15 +4,11 @@ const Component3 = (props) => {
       <div className="h-[75px]"></div>
       <img
         className="object-cover h-[350px] w-[1400px]"
-        src="https://github.com/w87895wen/React-Project/blob/main/vite-project/images/paper1(1100%20x%20400%20px).png?raw=true"
+        src="https://github.com/w7978708wen/React-Project/blob/main/vite-project/images/paper(1100%20x%20400%20px).png?raw=true"
       />
       <img
         className="object-cover h-[200px] w-[1400px]"
         src="https://github.com/w87895wen/React-Project/blob/main/vite-project/images/stars_v4(800%20x%20300%20px).png?raw=true"
-      />
-      <img
-        className="object-cover h-[350px] w-[1400px]"
-        src="https://github.com/w87895wen/React-Project/blob/main/vite-project/images/paper2(1100%20x%20400%20px).png?raw=true"
       />
     </div>
   );
